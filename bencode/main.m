5ab50b5cbad304c08c829140c5cb1f48038a7aeb
Bryce Davis <BAD1128@gmail.com>
2014-02-27 19:15:40 -0500

    Initial Commit

---
+++ bencode/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  bencode
+//
+//  Created by Bryce Davis on 2/27/14.
+//  Copyright (c) 2014 Bryce Davis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
