b01e9bf8ca3945cc060e9f095058421d70d0a632
Bryce Davis <BAD1128@gmail.com>
2014-02-27 20:01:15 -0500

    Add stub testing class

---
+++ BencodeTester.h
@@ -0,0 +1,18 @@
+//
+//  BencodeTester.h
+//  bencode
+//
+//  Created by Bryce Davis on 2/27/14.
+//  Copyright (c) 2014 Bryce Davis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Bencode.h"
+
+@interface BencodeTester : NSObject
+
++ (BOOL) testStringDecode;
+
++ (void) runTests;
+
+@end
---
+++ BencodeTester.m
@@ -0,0 +1,22 @@
+//
+//  BencodeTester.m
+//  bencode
+//
+//  Created by Bryce Davis on 2/27/14.
+//  Copyright (c) 2014 Bryce Davis. All rights reserved.
+//
+
+#import "BencodeTester.h"
+
+@implementation BencodeTester
+
++ (BOOL) testStringDecode {
+    
+    return NO;
+}
+
++ (void) runTests {
+    
+}
+
+@end
