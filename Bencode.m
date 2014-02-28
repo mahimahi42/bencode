58d59f6de2943f41b7e435bb7fe2862a1e2a5068
Bryce Davis <BAD1128@gmail.com>
2014-02-27 19:57:34 -0500

    Fix bencode string decoder implementation

--- Bencode.m
+++ Bencode.m
@@ -10,25 +10,44 @@
 
 @implementation Bencode
 
-- (NSString*) decodeString:(NSString *)str {
-    NSString* retStr;
+- (BOOL) isNumber:(NSString *)str {
+    NSCharacterSet* nonNums = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    NSRange r = [str rangeOfCharacterFromSet:nonNums];
+    return r.location == NSNotFound;
+}
+
+- (NSString*) decodeString:(NSString *)str {    
+//    NSError* errRegex = NULL;
+//    NSRegularExpression* regex = [NSRegularExpression
+//                                  regularExpressionWithPattern:@"([\\d]+):[\\w]{$1}"
+//                                  options:NSRegularExpressionCaseInsensitive
+//                                  error:&errRegex];
+//    
+//    NSTextCheckingResult* searchStr = [regex firstMatchInString:str
+//                                             options:0
+//                                             range:NSMakeRange(0, [str length])];
+//    retStr = [str substringWithRange:searchStr.range];
+//    
+//    NSLog(@"ArgStr: %@ RetStr: %@", str, retStr);
+//    
+//    if ([retStr isEqualToString:str]) {
+//        return retStr;
+//    }
+    NSArray* strComponents = [str componentsSeparatedByString:@":"];
+    NSString* strLength = strComponents[0];
+    NSString* strContent = strComponents[1];
     
-    NSError* errRegex = NULL;
-    NSRegularExpression* regex = [NSRegularExpression
-                                  regularExpressionWithPattern:@"([\\d]+):[\\w]{$1}"
-                                  options:NSRegularExpressionCaseInsensitive
-                                  error:&errRegex];
-    
-    NSTextCheckingResult* searchStr = [regex firstMatchInString:str
-                                             options:0
-                                             range:NSMakeRange(0, [str length])];
-    retStr = [str substringWithRange:searchStr.range];
-    
-    if ([retStr isEqualToString:str]) {
-        return retStr;
+    if (![self isNumber:strLength]) {
+        return nil;
     }
     
-    return nil;
+    NSInteger strLen = [strLength integerValue];
+    
+    if (strLen != [strContent length]) {
+        return nil;
+    }
+    
+    return strContent;
 }
 
 @end
